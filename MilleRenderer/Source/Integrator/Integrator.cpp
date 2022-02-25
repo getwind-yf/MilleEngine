4757b982d62ac217ee6f721ac18c40c9c7e3a1db
dongyf <dyao1111@163.com>
2022-02-25 09:57:08 +0800

    Added Integrator

--- MilleRenderer/Source/Integrator/Integrator.cpp
+++ MilleRenderer/Source/Integrator/Integrator.cpp
@@ -178,12 +178,95 @@ namespace MILLE
 
 		Color f = pBSDF->SampleScattered(vOut, Sample(), diffGeom, &vIn, &pdf, ScatterType(BSDF_REFLECTION | BSDF_SPECULAR)); 
 
+		// Differential Ray 
 		Color color; 
 		if (pdf > 0.0f && !f.IsBlack() && Math::AbsDot(vIn, normal) != 0.0f)
 		{
-			RayDifferential rayRef = RayDifferential(position, vIn, diffGeom)
+			RayDifferential rayRef = RayDifferential(position, vIn, diffGeom.mMediumInterface.GetMedium(vIn, normal), float(Math::EDX_INFINITY), 0.0f, ray.mDepth + 1); 
+
+			if (ray.mHasDifferential)
+			{
+				rayRef.mHasDifferential = true; 
+				rayRef.mDxOrg = position + diffGeom.mDpdx;
+				rayRef.mDyOrg = position + diffGeom.mDpdy; 
+
+				Vector3 vDndx = diffGeom.mDndu * diffGeom.mDudx + diffGeom.mDndv * diffGeom.mDvdx; 
+				Vector3 vDndy = diffGeom.mDndu * diffGeom.mDudy + diffGeom.mDndv * diffGeom.mDvdy; 
+
+				Vector3 vDOutdx = -ray.mDxDir - vOut; 
+				Vector3 vDOutdy = -ray.mDyDir - vOut; 
+
+				float fDcosdx = Math::Dot(vDOutdx, normal) + Math::Dot(vOut, vDndx); 
+				float fDcosdy = Math::Dot(vDOutdy, normal) + Math::Dot(vOut, vDndy); 
+
+				rayRef.mDxDir = vIn - vDOutdx + 2.0f * (Math::Dot(vOut, normal) * vDndx + fDcosdx * normal); 
+				rayRef.mDyDir = vIn - vDOutdy + 2.0f * (Math::Dot(vOut, normal) * vDndy + fDcosdy * normal); 
+			}
+
+			Color L = pIntegrator->Li(rayRef, pScene, pSampler, random, memory); 
+			color = f * L * Math::AbsDot(vIn, normal) / pdf; 
 		}
 
+		return color; 
+	}
+
+	// Scene, Sampler, differentialray, differetialGeom, RandomGen, MemoryPool 
+	Color Integrator::SpecularTransmit(const TiledIntegrator* pIntegrator, 
+		const Scene* pScene, 
+		Sampler* pSampler, 
+		const RayDifferential& ray, 
+		const DifferentialGeom& diffGeom, 
+		RandomGen& random, 
+		MemoryPool& memory
+		)
+	{
+		const Vector3& position = diffGeom.mPosition;
+		const Vector3& normal = diffGeom.mNormal;
+		const BSDF* pBSDF = diffGeom.mpBSDF;
+		Vector3 vOut = -ray.mDir, vIn;
+		float pdf;
+
+		Color f = pBSDF->SampleScattered(vOut, Sample(), diffGeom, &vIn, &pdf, ScatterType(BSDF_TRANSMISSION | BSDF_SPECULAR));
+
+		Color color;
+		if (pdf > 0.0f && !f.IsBlack() && Math::AbsDot(vIn, normal) != 0.0f)
+		{
+			RayDifferential rayRfr = RayDifferential(position, vIn, diffGeom.mMediumInterface.GetMedium(vIn, normal), float(Math::EDX_INFINITY), 0.0f, ray.mDepth + 1);
+			if (ray.mHasDifferential)
+			{
+				rayRfr.mHasDifferential = true;
+				rayRfr.mDxOrg = position + diffGeom.mDpdx;
+				rayRfr.mDyOrg = position + diffGeom.mDpdy;
+
+				float fEta = 1.5f;
+				Vector3 vD = -vOut;
+				if (Math::Dot(vOut, normal) < 0.0f)
+					fEta = 1.0f / fEta;
+
+				Vector3 vDndx = diffGeom.mDndu * diffGeom.mDudx +
+					diffGeom.mDndv * diffGeom.mDvdx;
+				Vector3 vDndy = diffGeom.mDndu * diffGeom.mDudy +
+					diffGeom.mDndv * diffGeom.mDvdy;
+
+				Vector3 vDOutdx = -ray.mDxDir - vOut;
+				Vector3 vDOutdy = -ray.mDyDir - vOut;
+
+				float fDcosdx = Math::Dot(vDOutdx, normal) + Math::Dot(vOut, vDndx);
+				float fDcosdy = Math::Dot(vDOutdy, normal) + Math::Dot(vOut, vDndy);
+
+				float fMu = fEta * Math::Dot(vD, normal) - Math::Dot(vIn, normal);
+				float dfMudx = (fEta - (fEta * fEta * Math::Dot(vD, normal)) / Math::Dot(vIn, normal)) * fDcosdx;
+				float dfMudy = (fEta - (fEta * fEta * Math::Dot(vD, normal)) / Math::Dot(vIn, normal)) * fDcosdy;
+
+				rayRfr.mDxDir = vIn + fEta * vDOutdx - (fMu * vDndx + dfMudx * normal);
+				rayRfr.mDyDir = vIn + fEta * vDOutdy - (fMu * vDndy + dfMudy * normal);
+			}
+
+			Color L = pIntegrator->Li(rayRfr, pScene, pSampler, random, memory);
+			color = f * L * Math::AbsDot(vIn, normal) / pdf;
+		}
+
+		return color;
 	}
 
 }
