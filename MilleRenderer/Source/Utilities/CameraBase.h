0f3068cb94ffe4edf8f5b77146d78ce6ebdbbd97
dongyf <dyao1111@163.com>
2022-02-28 14:06:36 +0800

    Camerabase

--- MilleRenderer/Source/Utilities/CameraBase.h
+++ MilleRenderer/Source/Utilities/CameraBase.h
@@ -5,6 +5,36 @@ class Camera
 public:
 	Vector3 mPos; 
 	Vector3 mTarget; 
-	Vector3 
+	Vector3 mUp; 
+	Vector3 mDir; 
+
+	float mMoveScaler; 
+	float mRotateScaler; 
+	float mYaw, mPitch; 
+
+	float mFOV; 
+	float mRatio; 
+	float mNearClip; 
+	float mFarClip; 
+
+protected: 
+	float mFOV_2; 
+
+	int mFilmResX; 
+	int mFilmResY; 
+
+	// Matrices 
+	Matrix mView;
+	Matrix mViewInv; 
+	Matrix mProj; 
+
+	Matrix mScreenToRaster; 
+	Matrix mRasterToCamera; 
+
+public:
+	Camera(); 
+	Camera(const Vector3& ptPos, const Vector3& ptTar, const Vector3& vUp, int iResX, int iResY,
+		float fFOV = 35.0f, float fNear = 0.1f, float fFar = 1000.0f);
+
 
 };
