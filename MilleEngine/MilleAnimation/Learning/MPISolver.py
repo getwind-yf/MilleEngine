f74a43f331557edab01e243eec4b56b0c9478dc6
dongyf <dyao1111@163.com>
2022-05-06 19:24:46 +0800

    Update

--- MilleEngine/MilleAnimation/Learning/MPISolver.py
+++ MilleEngine/MilleAnimation/Learning/MPISolver.py
@@ -23,7 +23,6 @@ class MPISolver(Sovler):
 	def get_stepsize(self):
 		return self.optimizer._learning_rate_tensor.eval()
 
-
 	def update(self, grads=None, grad_scale=1.0):
 		if grads is not None:
 			self._flat_grad = MathUtil.flatten(grads) 
@@ -31,7 +30,6 @@ class MPISolver(Sovler):
 			self._flat_grad.fill(0)
 		return self.update_flatgrad(self._flat_grad, grad_scale)
 		pass
-
 
 	def update_flatgrad(self):
 		if self.iter % self.CHECK_SYNC_ITERS == 0:
@@ -41,24 +39,51 @@ class MPISolver(Sovler):
 		MPI.COMM_WORLD.Allreduce(flat_grad, self._global_flat_grad, op=MPI.SUM)
 		self._global_flat_grad /= MPIUtil.get_num_procs() 
 		self._load_flat_grad(self._global_flat_grad) 
-		self.sess.run()
-		pass
+		self.sess.run([self._update], self._grad_feed) 
+		self.iter += 1 
+		return 
 
 	def sync(self):
-		pass
+		vars = self._get_flat_vars() 
+		MPIUtil.bcast(vars)
+		self._set_flat_vars(vars)
+		return 
 
 	def check_synced(self):
-		pass
+		synced = True 
+		if self._is_root(): 
+			vars = self._get_flat_vars() 
+			MPIUtil.bcast(vars_root)
+			synced = (vars_local == vars_root).all() 
+		return synced 
 
 	def _is_root(self):
-		pass
+		return MPIUtil.is_root_proc() 
 
 	def _build_grad_feed(self):
-		pass
+		self._grad_tf_list = [] 
+		self._grad_buffers = [] 
+		for v in self.vars: 
+			shape = v.get_shape() 
+			grad = np.zeros(shape)
+			grad_tf = tf.placeholder(tf.float32, shape=shape)
+			self._grad_buffers.append(grad)
+			self._grad_tf_list.append(grad_tf)
+		self._grad_feed = dict({g_tf: g for g_tf, g in zip(self._grad_tf_list, self._grad_buffers)})
+		return 
 
 	def _calc_grad_dim(self):
-		pass
+		grad_dim = 0 
+		for grad in self._grad_buffers:
+			grad_dim += grad.size 
+		return grad_dim 
 
-	def _load_flat_grad(self):
+	def _load_flat_grad(self, flat_grad):
+		start = 0
+		for g in self._grad_buffers:
+			size = g.size
+			np.copyto(g, np.reshape(flat_grad[start:start + size], g.shape))
+			start += size  
+		return 
 		pass 
 
