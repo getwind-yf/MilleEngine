d022fc90e5b0eaa514690e73a4b900b65b7da752
dongyf <dyao1111@163.com>
2022-05-06 19:11:43 +0800

    Update Solver and layer

--- MilleEngine/MilleAnimation/Learning/MPISolver.py
+++ MilleEngine/MilleAnimation/Learning/MPISolver.py
@@ -24,11 +24,24 @@ class MPISolver(Sovler):
 		return self.optimizer._learning_rate_tensor.eval()
 
 
-	def update(self):
+	def update(self, grads=None, grad_scale=1.0):
+		if grads is not None:
+			self._flat_grad = MathUtil.flatten(grads) 
+		else:
+			self._flat_grad.fill(0)
+		return self.update_flatgrad(self._flat_grad, grad_scale)
 		pass
 
 
 	def update_flatgrad(self):
+		if self.iter % self.CHECK_SYNC_ITERS == 0:
+			assert self.check_synced(), Logger.print("Network parameters desynchronized")
+		if grad_scale != 1.0: 
+			flat_grad *= grad_scale
+		MPI.COMM_WORLD.Allreduce(flat_grad, self._global_flat_grad, op=MPI.SUM)
+		self._global_flat_grad /= MPIUtil.get_num_procs() 
+		self._load_flat_grad(self._global_flat_grad) 
+		self.sess.run()
 		pass
 
 	def sync(self):
